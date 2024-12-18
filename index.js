022e692da7b51f2c6466661fc9adaec6213a1792
Pierre Valadeau <p.valadeau@outlook.fr>
2024-12-18 10:49:32 +0100

    creat dictionnary about extensions and files

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const path = require('patch'); // Module pour gérer les chemins de fichiers
 
 const dowloadsFolder = path.join(process.env.HOME, 'Téléchargements'); // Chemin vers le dossier téléchargements
 const desktopFolder = path.join(process.env.HOME, 'Bureau'); // Chemin vers le bureau
+
+const fileTypes = {
+  images: ['jpg', 'jpeg', 'png', 'gif'], 
+  documents: ['pdf', 'doc', 'docx', 'txt'], 
+  videos: ['mp4', 'mkv', 'avi'], 
+  music: ['mp3', 'wav', 'flac'], 
+}
