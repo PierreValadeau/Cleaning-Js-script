c8f3b68f7c822894970b3130c07f3cec456b6970
Pierre Valadeau <p.valadeau@outlook.fr>
2024-12-18 10:53:29 +0100

    create function to get file extension

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ const path = require('patch'); // Module pour gérer les chemins de fichiers
 const dowloadsFolder = path.join(process.env.HOME, 'Téléchargements'); // Chemin vers le dossier téléchargements
 const desktopFolder = path.join(process.env.HOME, 'Bureau'); // Chemin vers le bureau
 
+// Dictionnaire des extensions et dossiers cibles
 const fileTypes = {
   images: ['jpg', 'jpeg', 'png', 'gif'], 
   documents: ['pdf', 'doc', 'docx', 'txt'], 
   videos: ['mp4', 'mkv', 'avi'], 
   music: ['mp3', 'wav', 'flac'], 
+}
+
+// Fonction pour obtenir l'extension d'un fichier 
+
+const getFilType = (filename) => {
+  const ext = path.extname(filename).slice(1).toLowerCase(); // transforme .jpg en jpg 
+  for (const [type, extensions] of Object.entries(fileTypes)) {
+    if (extensions.includes(ext)) return type; 
+  }
+  return 'others'; // Fichiers qui ne rentrent pas dans les catégories 
 }
